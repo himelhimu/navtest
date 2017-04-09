5258c2f19c4f146fd449e170da8b616f8aec2983
sabbir <sabbirahmedhimel@gmail.com>
2017-04-09 14:00:43 +0600

    added ColapsingToolbar in Image Details view

--- app/src/main/java/test/android/sabbir/navdrawer/activites/NasaPhotoDetailsActivity.java
+++ app/src/main/java/test/android/sabbir/navdrawer/activites/NasaPhotoDetailsActivity.java
@@ -1,7 +1,9 @@
 package test.android.sabbir.navdrawer.activites;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,17 +23,23 @@ public class NasaPhotoDetailsActivity extends AppCompatActivity {
     TextView tvTittle;
     @BindView(R.id.details)
     TextView tvDetails;
+    @BindView(R.id.colapssing_toolbar)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @BindView(R.id.toolbar_details)
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nasa_photo_details);
         ButterKnife.bind(this);
 
+        setSupportActionBar(mToolbar);
         NasaPhoto nasaPhoto= (NasaPhoto) getIntent().getSerializableExtra("photo");
         if (nasaPhoto!=null){
             Picasso.with(this).load(nasaPhoto.getUrl()).into(imageView);
             textViewDate.setText(nasaPhoto.getDate());
             tvTittle.setText(nasaPhoto.getTitle());
+            mCollapsingToolbarLayout.setTitle(nasaPhoto.getTitle());
             tvDetails.setText(nasaPhoto.getExplanation());
         }
 
